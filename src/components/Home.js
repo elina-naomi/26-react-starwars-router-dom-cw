0101fadc0b3a3e24054adea2ce17577a1bbeb873
elina-naomi <elina-naomi@mail.ru>
2020-11-04 08:52:14 +0200

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Hero from "./Hero";
+import DreamTeam from "./DreamTeam";
+import FarGalaxy from "./FarGalaxy";
+import {defaultHero, friends} from "../utils/Constants";
+import {Redirect} from "react-router-dom";
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            existHero: true,
+            hero: defaultHero
+        }
+    }
+
+    getHero = () => {
+        let existHero = true;
+        let hero = this.props.match.params.hero||defaultHero;
+        if (!friends.includes(hero)) {
+            hero = defaultHero;
+            existHero = false;
+        }
+        this.setState({existHero, hero});
+    }
+
+    componentDidMount() {
+        this.getHero();
+        this.props.changeHero(this.state.hero);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.hero !== prevProps.match.params.hero){
+            this.getHero();
+            this.props.changeHero(this.state.hero);
+        }
+    }
+
+    render() {
+        if (this.state.existHero){
+            return (
+                <main className="clearfix">
+                    <Hero hero={this.state.hero}/>
+                    <DreamTeam hero={this.state.hero}/>
+                    <FarGalaxy/>
+                </main>
+            );
+        }else{
+            return <Redirect to={`/error`}/>
+        }
+
+    }
+}
+
+export default Home;
